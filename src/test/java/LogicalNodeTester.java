9dccf8d6394b9870e8a08aaa8504285cf60bd634
RyanKruger1 <ryankruger45@gmail.com>
2021-08-28 13:56:51 +0200

    Update unit tests for data structures

--- src/test/java/LogicalNodeTester.java
+++ src/test/java/LogicalNodeTester.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import Structures.LogicalNode;
 import org.w3c.dom.NodeList;
@@ -7,5 +8,40 @@ import utilities.XMLReader;
 public class LogicalNodeTester {
 
     XMLReader xml = new XMLReader();
+    static LogicalNode logicalNode;
 
+    @Before
+    public void setUp() throws Exception{
+        NodeList nodeList = xml.getLogicalNodes();
+         logicalNode =
+                new LogicalNode(
+                        XMLReader.getDataObjects(nodeList.item(0)),
+                        XMLReader.getAttributeValue(nodeList.item(0),"lnType"),
+                        XMLReader.getAttributeValue(nodeList.item(0),"lnClass"),
+                        XMLReader.getAttributeValue(nodeList.item(0),"name"));
+    }
+
+    @Test
+    public void assertAmountOfDataObjectsInLogicalNode(){
+        System.out.println(logicalNode.getDataObjects().length);
+        Assert.assertNotNull(logicalNode.getDataObjects().length);
+    }
+
+    @Test
+    public void assertFirstDataAttributeAmount(){
+        System.out.println(logicalNode.getDataObjects()[0].getAttributes().length);
+        Assert.assertNotNull(logicalNode.getDataObjects()[0].getAttributes().length);
+    }
+
+    @Test
+    public void assertThatDataObjectNamesAreRetrievable(){
+        System.out.println(logicalNode.getDataObjects()[0].getName());
+        Assert.assertNotNull(logicalNode.getDataObjects()[0].getName());
+    }
+
+    @Test
+    public void assertThatAttributeNamesAreRetrievable(){
+        System.out.println(logicalNode.getDataObjects()[0].getAttributes()[0].getName());
+        Assert.assertNotNull(logicalNode.getDataObjects()[0].getAttributes()[0].getName());
+    }
 }
